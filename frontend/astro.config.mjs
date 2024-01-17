8d308d4c91d8c538e846eace0984041ff2d67890
corentings <contact@corentings.dev>
2024-01-17 20:33:06 +0100

    :sparkles: PWA !

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from "astro/config";
+import {defineConfig} from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
 import node from '@astrojs/node';
@@ -9,18 +9,67 @@ import compress from "astro-compress";
 // https://astro.build/config
 import compressor from "astro-compressor";
 
+import AstroPWA from '@vite-pwa/astro'
+
 
 // https://astro.build/config
 import critters from "astro-critters";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astrocket.corentings.dev",
-  integrations: [tailwind(), solidJs(), sitemap(), compress({
-    css: false
-  }),  critters(), compressor()],
-  output: "server",
-  adapter: node({
-    mode: 'standalone'
-  })
+    site: "https://astrocket.corentings.dev",
+    integrations: [tailwind(), solidJs(), sitemap(), compress({
+        css: false
+    }), critters(), compressor(), AstroPWA({
+        mode: 'development',
+        registerType: 'autoUpdate',
+        scope: '/',
+        base: '/',
+        includeAssets: [
+            'favicon.ico',
+        ],
+        manifest: {
+            name: 'Astrocket',
+            short_name: 'Astrocket',
+            theme_color: '#ffffff',
+            icons: [
+                {
+                    src: '/favicons/favicon-192x192.png',
+                    sizes: '192x192',
+                    type: 'image/png',
+                },
+                {
+                    src: '/favicons/favicon-512x512.png',
+                    sizes: '512x512',
+                    type: 'image/png',
+                },
+                {
+                    src: '/favicons/favicon-512x512.png',
+                    sizes: '512x512',
+                    type: 'image/png',
+                    purpose: 'any maskable',
+                },
+            ],
+        },
+        workbox: {
+            navigateFallback: '/404',
+            globPatterns: ['**/*.{css,js,html,svg,png,ico,txt}'],
+            globDirectory: 'dist',
+            globIgnores: [
+                '**/node_modules/**/*',
+                'sw.js',
+                'workbox-*.js',
+                '**/Avatar.tsx',
+            ]
+        },
+        devOptions: {
+            enabled: true,
+            navigateFallbackAllowlist: [/^\//],
+        },
+
+    })],
+    output: "server",
+    adapter: node({
+        mode: 'standalone'
+    })
 });
